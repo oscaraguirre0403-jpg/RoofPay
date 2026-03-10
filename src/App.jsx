07ce2bc280e1e9c9ec54b9420f98aadfc685c640
oscaraguirre0403-jpg <oscaraguirre0403@gmail.com>
2026-03-10 10:41:02 -0400

    Add main application component for RoofPay

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>RoofPay</h1>
+            <p>Your roofing subcontractor price list and job tracking app</p>
+        </div>
+    );
+};
+
+export default App;
